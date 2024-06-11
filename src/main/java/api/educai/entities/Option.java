eca2d5fe453e5dbfb5bbf7b534f1430be9544e0c
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2024-06-11 13:21:24 -0300

    Revert "add-unit-tests"

--- src/main/java/api/educai/entities/Option.java
+++ src/main/java/api/educai/entities/Option.java
@@ -2,17 +2,13 @@ package api.educai.entities;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
-@AllArgsConstructor
-@NoArgsConstructor
 public class Option {
 
     @Id
