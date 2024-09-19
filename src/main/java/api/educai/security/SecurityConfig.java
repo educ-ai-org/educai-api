7bf7b138d53cdb6b029d6b8c9a85654be8cbe175
Diego de Sousa <diegosousa241104@gmail.com>
2024-09-19 01:00:39 -0300

    Update SecurityConfig.java

--- src/main/java/api/educai/security/SecurityConfig.java
+++ src/main/java/api/educai/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:80", "http://20.51.193.153", "http://educai.eastus.cloudapp.azure.com", "https://educai.eastus.cloudapp.azure.com"));
+        configuration.setAllowedOriginPatterns("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "userId", "classworkId", "classroomId"));
         configuration.setExposedHeaders(List.of("x-auth-token"));
