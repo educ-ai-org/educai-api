fbc07d76943e38e93ee71696e9239d295907942a
Diego de Sousa <diegosousa241104@gmail.com>
2024-09-19 01:04:56 -0300

    Update SecurityConfig.java

--- src/main/java/api/educai/security/SecurityConfig.java
+++ src/main/java/api/educai/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns("*");
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "userId", "classworkId", "classroomId"));
         configuration.setExposedHeaders(List.of("x-auth-token"));
