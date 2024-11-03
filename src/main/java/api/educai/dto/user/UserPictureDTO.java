3e75d909b47c7e545640ebd1a775126ed51030fc
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2024-11-03 12:39:23 -0300

    feat: return user id as string

--- src/main/java/api/educai/dto/user/UserPictureDTO.java
+++ src/main/java/api/educai/dto/user/UserPictureDTO.java
@@ -3,16 +3,15 @@ package api.educai.dto.user;
 import api.educai.entities.User;
 import api.educai.services.AzureBlobService;
 import lombok.Data;
-import org.bson.types.ObjectId;
 
 @Data
 public class UserPictureDTO {
 
-    private ObjectId id;
+    private String id;
     private String profilePicture;
 
     public UserPictureDTO(User user) {
-        this.id = user.getId();
+        this.id = user.getId().toString();
         this.profilePicture = user.getProfilePicture() + "?" + AzureBlobService.getGlobalToken();
     }
 
